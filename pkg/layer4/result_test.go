6ae3312f5e5a102eaac6cdb61c457bceb343262a
Travis Truman <trumant@gmail.com>
2025-04-06 12:29:59 -0400

    test: adding unit testing for layer4.Result#UpdateAggregateResult func (#15)
    * Updating .gitignore match for binaries
    
    The match needs to be specific to the rootdir
    
    Signed-off-by: Travis Truman <trumant@gmail.com>
    
    * Adding unit test coverage for layer4.Result#UpdateAggregateResult
    
    Signed-off-by: Travis Truman <trumant@gmail.com>
    
    ---------
    
    Signed-off-by: Travis Truman <trumant@gmail.com>

--- pkg/layer4/result_test.go
+++ pkg/layer4/result_test.go
@@ -45,3 +45,64 @@ func TestResultString(t *testing.T) {
 		})
 	}
 }
+
+func TestUpdateAggregateResult(t *testing.T) {
+	tests := []struct {
+		name     string
+		prev     Result
+		new      Result
+		expected Result
+	}{
+		{
+			name:     "NotRun should not overwrite anything",
+			prev:     Passed,
+			new:      NotRun,
+			expected: Passed,
+		},
+		{
+			name:     "Failed should not be overwritten by anything",
+			prev:     Failed,
+			new:      Passed,
+			expected: Failed,
+		},
+		{
+			name:     "Failed should overwrite anything",
+			prev:     Passed,
+			new:      Failed,
+			expected: Failed,
+		},
+		{
+			name:     "Unknown should not be overwritten by NeedsReview",
+			prev:     Unknown,
+			new:      NeedsReview,
+			expected: Unknown,
+		},
+		{
+			name:     "Unknown should not be overwritten by Passed",
+			prev:     Unknown,
+			new:      Passed,
+			expected: Unknown,
+		},
+		{
+			name:     "NeedsReview should not be overwritten by Passed",
+			prev:     NeedsReview,
+			new:      Passed,
+			expected: NeedsReview,
+		},
+		{
+			name:     "NeedsReview should overwrite Passed",
+			prev:     Passed,
+			new:      NeedsReview,
+			expected: NeedsReview,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := UpdateAggregateResult(test.prev, test.new)
+			if actual != test.expected {
+				t.Errorf("expected %s, got %s", test.expected, actual)
+			}
+		})
+	}
+}
