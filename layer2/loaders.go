5ea8d760820a573aded5200ab1ae58be7a23b715
Eddie Knight <knight@linux.com>
2025-09-07 07:36:15 -0500

    feat: Added layer2 loader LoadNestedCatalog (#119)
    * Added new layer 2 loader and tests
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * refining tests
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * Applied suggestions from @trumant
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    ---------
    
    Signed-off-by: Eddie Knight <knight@linux.com>

--- layer2/loaders.go
+++ layer2/loaders.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 
@@ -90,6 +91,81 @@ func (c *Catalog) LoadFile(sourcePath string) error {
 	} else {
 		return fmt.Errorf("unsupported file type")
 	}
+	return nil
+}
+
+// LoadNestedCatalog loads a YAML file containing a nested catalog.
+// Only supports a single layer of nesting.
+// Accepts file URIs with the 'file:///' prefix.
+// Throws an error if the URL is not https.
+// TODO: Consider validating/sanitizing inputs to reduce injection risks.
+func (c *Catalog) LoadNestedCatalog(sourcePath, fieldName string) error {
+	if fieldName == "" {
+		return fmt.Errorf("fieldName cannot be empty")
+	}
+
+	var yamlData map[string]interface{}
+
+	parsedURL, err := url.Parse(sourcePath)
+	if err != nil {
+		return fmt.Errorf("failed to parse sourcePath: %w", err)
+	}
+
+	switch parsedURL.Scheme {
+	case "http":
+		// Reject http URLs for security reasons, only allow https/file.
+		return fmt.Errorf("insecure http URLs are not allowed: %s", sourcePath)
+	case "https":
+		// TODO: Validate URLs to avoid SSRF and other external resource injection risks.
+		resp, err := http.Get(sourcePath)
+		if err != nil {
+			return fmt.Errorf("failed to fetch URL: %v", err)
+		}
+		defer func() {
+			_ = resp.Body.Close()
+		}()
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("failed to fetch URL; response status: %v", resp.Status)
+		}
+		decoder := yaml.NewDecoder(resp.Body)
+		err = decoder.Decode(&yamlData)
+		if err != nil {
+			return fmt.Errorf("failed to decode YAML from URL: %v", err)
+		}
+	case "file":
+		// TODO: Sanitize file paths to prevent directory traversal and unauthorized access.
+		localPath := parsedURL.Path
+		file, err := os.Open(localPath)
+		if err != nil {
+			return fmt.Errorf("error opening file: %w", err)
+		}
+		defer func() {
+			_ = file.Close()
+		}()
+		decoder := yaml.NewDecoder(file)
+		err = decoder.Decode(&yamlData)
+		if err != nil {
+			return fmt.Errorf("error decoding YAML: %w (%s)", err, localPath)
+		}
+	default:
+		return fmt.Errorf("unsupported sourcePath scheme in %s: %s", parsedURL.Path, parsedURL.Scheme)
+	}
+
+	// TODO: Validate fieldName and the nested YAML content for injection risks.
+	fieldData, exists := yamlData[fieldName]
+	if !exists {
+		return fmt.Errorf("field '%s' not found in YAML file", fieldName)
+	}
+	fieldYamlBytes, err := yaml.Marshal(fieldData)
+	if err != nil {
+		return fmt.Errorf("error marshaling field data to YAML: %w", err)
+	}
+	decoder := yaml.NewDecoder(strings.NewReader(string(fieldYamlBytes)))
+	err = decoder.Decode(c)
+	if err != nil {
+		return fmt.Errorf("error decoding field '%s' into Catalog: %w", fieldName, err)
+	}
+
 	return nil
 }
 
--- layer2/loaders_test.go
+++ layer2/loaders_test.go
@@ -74,6 +74,81 @@ func Test_LoadFile(t *testing.T) {
 	}
 }
 
+func Test_LoadNestedCatalog(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Catalog{}
+			err := c.LoadNestedCatalog(tt.sourcePath, "")
+			if err == nil {
+				t.Errorf("Un-nested catalogs are expected to fail")
+			}
+		})
+	}
+
+	nestedTests := []struct {
+		name            string
+		sourcePath      string
+		nestedFieldName string
+		wantErr         bool
+	}{
+		{
+			name:            "Malformed URI",
+			sourcePath:      "https://",
+			nestedFieldName: "catalog",
+			wantErr:         true,
+		},
+		{
+			name:            "Non-conformant URI response",
+			sourcePath:      "https://google.com",
+			nestedFieldName: "catalog",
+			wantErr:         true,
+		},
+		{
+			name:            "Local file does not exist",
+			sourcePath:      "wonky-file-name.yaml",
+			nestedFieldName: "catalog",
+			wantErr:         true,
+		},
+		{
+			name:            "Empty nested catalog",
+			sourcePath:      "./test-data/nested-empty.yaml",
+			nestedFieldName: "catalog",
+			wantErr:         true,
+		},
+		{
+			name:            "Nested field name not provided",
+			sourcePath:      "./test-data/nested-good-ccc.yaml",
+			nestedFieldName: "",
+			wantErr:         true,
+		},
+		{
+			name:            "Nested field name not present in target file",
+			sourcePath:      "./test-data/nested-good-ccc.yaml",
+			nestedFieldName: "doesnt-exist",
+			wantErr:         true,
+		},
+	}
+
+	for _, tt := range nestedTests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Catalog{}
+			err := c.LoadNestedCatalog(tt.sourcePath, tt.nestedFieldName)
+			if tt.wantErr && err == nil {
+				t.Errorf("Expected error, but got none")
+			} else if !tt.wantErr && err != nil {
+				t.Errorf("Did not expect error, but got '%s'", err.Error())
+			} else if !tt.wantErr {
+				if len(c.ControlFamilies) == 0 {
+					t.Errorf("Catalog.LoadControlFamily() did not load any control families")
+				} else if len(c.ControlFamilies) > 0 {
+					assert.NotEmpty(t, c.ControlFamilies[0].Title, "Control family title should not be empty")
+					assert.NotEmpty(t, c.ControlFamilies[0].Description, "Control family description should not be empty")
+				}
+			}
+		})
+	}
+}
+
 func Test_LoadFiles(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
